7fbf83505995776a2449084a074d24806dd51980
Romain Quetiez <romain.quetiez@combodo.com>
2016-07-07 09:00:30 +0000

    Brand new module, ready for developments
    SVN:trunk[5325]

---
+++ module.itop-communications.php
@@ -0,0 +1,52 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'itop-communications/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Communications in the portal',
+		'category' => 'portal',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-portal'
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.itop-communications.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
