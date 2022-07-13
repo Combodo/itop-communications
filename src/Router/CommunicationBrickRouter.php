0324ec065383a7906b70506b12b0775f98a29413
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2022-07-13 15:05:28 +0200

    N°5281 - Symfony 5.4 extensions controllers registration

--- src/Router/CommunicationBrickRouter.php
+++ src/Router/CommunicationBrickRouter.php
@@ -30,7 +30,7 @@ ItopExtensionsExtraRoutes::AddRoutes(
 /**
  * @since 3.1.0
  */
-if (function_exists('ItopExtensionsExtraRoutes::AddControllersClasses')) {
+if (method_exists(ItopExtensionsExtraRoutes::class, 'AddControllersClasses')) {
     /** @noinspection PhpUnhandledExceptionInspection */
     ItopExtensionsExtraRoutes::AddControllersClasses(
         array(
