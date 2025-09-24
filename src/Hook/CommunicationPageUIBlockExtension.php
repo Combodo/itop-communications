de804cb5797746abbd75ed011f31f6db0c91b2b9
Anne-Catherine <57360138+accognet@users.noreply.github.com>
2025-09-24 09:44:19 +0200

    Update src/Hook/CommunicationPageUIBlockExtension.php
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- src/Hook/CommunicationPageUIBlockExtension.php
+++ src/Hook/CommunicationPageUIBlockExtension.php
@@ -42,8 +42,8 @@ class CommunicationPageUIBlockExtension implements iPageUIBlockExtension
 			if ($oComm->IsUserInScope(UserRights::GetUserObject()) && $oComm->IsAllowedPortalsValid())
 			{
 				$iCount++;
-				$sTitle = MetaModel::ApplyParams($oComm->Get('title'), $aContext);
-				$sContent = MetaModel::ApplyParams($oComm->GetAsHTML('message'), $aContext);
+				$sTitle = MetaModel::ApplyParams($oComm->Get('title'), []);
+				$sContent = MetaModel::ApplyParams($oComm->GetAsHTML('message'), []);
 				$oAlertBlock = AlertUIBlockFactory::MakeForInformation($sTitle, $sContent, 'ibo-communication-'.$oComm->GetKey())
 					->EnableSaveCollapsibleState('ibo-communication-'.$oComm->GetKey())
 					->SetColor($oComm->GetColorFromIcon())
